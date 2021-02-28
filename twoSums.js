7b9d2ade19d0f157cb8dcf9618ecd9d7b46446cd
Darren Wong <darrenwong@darrens-mbp.lan>
2021-02-28 15:46:36 -0800

    add secondary answer thats more optimized if return is earlier

--- twoSums.js
+++ twoSums.js
@@ -41,3 +41,25 @@ const twoSum = (nums, target) => {
 
 console.log(twoSum([2,7,11,15], 9));
 
+
+// Twosumtwo is a better optimized solution because of how map.has method looks for the number
+// we can change that to if the complimentary is the index.
+
+//@jakesully don’t be happy. It’s because the expected solution is in the early half of the input test cases. So your solution returns early. If I change an example test case from [2,7,11,15], 9 to [11,15,2,7], 9, your solution takes 88ms and the original is 79ms.
+//Basically both are similar solution. You are saving the target – current (expected) and checking for its position, and others are saving position while checking for existence of target – current.
+//The 52ms is just a LeetCode one-off 🙂 (not consistently reproducible).
+
+
+//we can assume that if it returns early within the for loop its optimized. if it doesnt then the optimization would be the same as .map is fairly new to es6
+// .map for writing style and a bit more clear in terms of reading as a personal opinion
+const twoSumtwo = (nums, target) => {
+    let mapping = {};
+    for (let i = 0; i < nums.length; i++) {
+       if(mapping[nums[i]] >= 0) {
+           return [ mapping[nums[i]], i];
+       }
+       mapping[target-nums[i]] = i;
+    }
+};
+
+console.log(twoSumtwo([2,7,11,15], 9));
